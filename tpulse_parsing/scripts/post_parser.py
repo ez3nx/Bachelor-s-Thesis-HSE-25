9b997d1d021f66385dbecfbafa6f34ad22376cb9
Michael <yasons2003@gmail.com>
2025-04-15 17:29:09 +0300

    pep formated

--- tpulse_parsing/scripts/post_parser.py
+++ tpulse_parsing/scripts/post_parser.py
@@ -7,7 +7,7 @@ from time import sleep
 from datetime import datetime
 import logging
 
-from IPython.display import clear_output # type: ignore
+from IPython.display import clear_output  # type: ignore
 
 from scripts.database import batch_insert
 import dateutil.parser
@@ -25,14 +25,17 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 @dataclass
 class ErrorLog:
     ticker: str
     cursor: int
     error: Exception
 
+
 def get_dt():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 class TinkoffPulseParser:
     def __init__(self, start_date: str = None, db_name: str = None):
@@ -46,10 +49,10 @@ class TinkoffPulseParser:
         self.errors: List[ErrorLog] = []
         self.total_posts = 0
         self.rounds = 0
-        
+
         # Преобразуем строку даты в datetime объект с UTC
         self.start_date = (
-            datetime.strptime(start_date, '%Y-%m-%d').replace(tzinfo=UTC)
+            datetime.strptime(start_date, "%Y-%m-%d").replace(tzinfo=UTC)
             if start_date
             else None
         )
@@ -63,40 +66,40 @@ class TinkoffPulseParser:
         """Парсит посты для одного тикера"""
         if self.db_name is None:
             self.db_name = self._generate_db_name(ticker)
-        
+
         fetch_cursor = 999999999
-        
+
         logger.info(f"Парсинг постов по тикеру {ticker}...")
 
         while True:
             # logger.info(f"Прокрутка для тикера {ticker}")
             try:
                 posts_from_ticker = self.pulse.get_posts_by_ticker(
-                    ticker=ticker, 
-                    cursor=fetch_cursor
+                    ticker=ticker, cursor=fetch_cursor
                 )
                 posts = posts_from_ticker["items"]
-                
+
                 # Проверяем дату самого старого поста
                 oldest_post_date = self._parse_date(posts[-1]["inserted"])
-                
+
                 # Если дошли до постов раньше заданной даты, прерываем цикл
                 if self.start_date and oldest_post_date < self.start_date:
                     # Фильтруем только посты после start_date
                     posts = [
-                        post for post in posts
+                        post
+                        for post in posts
                         if self._parse_date(post["inserted"]) >= self.start_date
                     ]
                     if posts:  # Если остались посты после фильтрации
                         self._parse_posts_batch(posts)
                     logger.info(f"Достигнута целевая дата {self.start_date}")
                     break
-                
+
                 self._parse_posts_batch(posts)
                 fetch_cursor = posts_from_ticker["nextCursor"]
                 # logger.info(f"Получено постов: {len(posts)}")
                 sleep(randint(2, 5))
-                
+
             except (HTTPStatusError, ConnectError, ConnectTimeout, ReadTimeout) as e:
                 logger.error("Network error", exc_info=e)
                 sleep(25)
@@ -146,11 +149,12 @@ class TinkoffPulseParser:
         """Парсит посты для списка тикеров из файла"""
         with open(filename, "r") as f:
             tickers = [i.strip() for i in f.read().split("\n")]
-        
+
         for ticker in tickers:
             self.parse_single_ticker(ticker)
-        
+
         return self.errors
+
 
 if __name__ == "__main__":
     parser = TinkoffPulseParser()
